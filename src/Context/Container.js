03fa2bda0cfee56b0d02a09c2a9191fb29945255
ademten82 <ademten7@gmail.com>
2021-11-30 15:51:03 +0100

    updated Login.js

--- src/Context/Container.js
+++ src/Context/Container.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { MyContext } from "./context";
 import { array } from "../data";
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
 
 const Container = (props) => {
   const [user, setUser] = useState(null);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { initializeApp } from "firebase/app";
+import { app } from "../Context/Container";
 import {
   getAuth,
   GoogleAuthProvider,
@@ -10,19 +10,10 @@ import {
 } from "firebase/auth";
 import GoogleButton from "react-google-button";
 import { MyContext } from "../Context/context";
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
-initializeApp(firebaseConfig);
-const auth = getAuth();
-const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
   const { setUser } = useContext(MyContext);
   //to change the url paramater
   const navigate = useNavigate();
